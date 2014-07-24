d1ec33595039f94086938e12ff2108fee85655b2
Lewis Jang <Lewis.AEdes@gmail.com>
2014-07-24 09:05:04 +0800

    add address all entity crud

---
+++ src/Aedes/AddressBundle/Form/AreaType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Aedes\AddressBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AreaType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('countryId')
+            ->add('cityId')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Aedes\AddressBundle\Entity\Area'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'aedes_addressbundle_area';
+    }
+}
---
+++ src/Aedes/AddressBundle/Form/CountryType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Aedes\AddressBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CountryType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('icon')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Aedes\AddressBundle\Entity\Country'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'aedes_addressbundle_country';
+    }
+}
