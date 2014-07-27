f21ab2245979d3a567ba1295bf98f6ef0d8b6c1f
Lewis Jang <Lewis.AEdes@gmail.com>
2014-07-28 07:34:58 +0800

    Bugfixed 補上 menu builder

---
+++ src/Aedes/MotoBundle/Menu/Builder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Aedes\MotoBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
+class Builder extends ContainerAware
+{
+    protected $factory;
+    protected $securityContext;
+
+    /**
+     * @param FactoryInterface         $factory
+     * @param SecurityContextInterface $securityContext
+     */
+    public function __construct(FactoryInterface $factory, SecurityContextInterface $securityContext) {
+        $this->factory = $factory;
+        $this->securityContext = $securityContext;
+    }
+
+    public function createMainMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('買機車', array('route' => 'moto'));
+        $menu->addChild('刊登機車', array('route' => 'moto_new'));
+
+        $isLogin = $this->securityContext->isGranted('IS_AUTHENTICATED_FULLY');
+
+        if (! $isLogin)
+        {
+            $menu->addChild('註冊', array('route' => 'fos_user_registration_register'));
+            $menu->addChild('登入', array('route' => 'fos_user_security_login'));
+        }
+        else
+        {
+            $menu->addChild('會員資料', array('route' => 'fos_user_profile_edit'));
+            $menu->addChild('登出', array('route' => 'fos_user_security_logout'));
+        }
+
+        return $menu;
+    }
+}
