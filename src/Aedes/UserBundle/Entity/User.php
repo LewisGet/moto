545c9e7b4511a24ff18176589b1ebbe1aaf70add
Lewis Jang <Lewis.AEdes@gmail.com>
2014-07-28 10:31:36 +0800

    更新使用者資料

--- src/Aedes/UserBundle/Entity/User.php
+++ src/Aedes/UserBundle/Entity/User.php
@@ -18,9 +18,129 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mobile", type="string", length=25)
+     */
+    private $mobile;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone", type="string", length=25)
+     */
+    private $phone;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contact", type="text")
+     */
+    private $contact;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ezTime", type="string", length=255)
+     */
+    private $ezTime;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Set mobile
+     *
+     * @param string $mobile
+     * @return User
+     */
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+
+        return $this;
+    }
+
+    /**
+     * Get mobile
+     *
+     * @return string
+     */
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     * @return User
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set contact
+     *
+     * @param string $contact
+     * @return User
+     */
+    public function setContact($contact)
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    /**
+     * Get contact
+     *
+     * @return string
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+     * Set ezTime
+     *
+     * @param string $ezTime
+     * @return User
+     */
+    public function setEzTime($ezTime)
+    {
+        $this->ezTime = $ezTime;
+
+        return $this;
+    }
+
+    /**
+     * Get ezTime
+     *
+     * @return string
+     */
+    public function getEzTime()
+    {
+        return $this->ezTime;
     }
 }
