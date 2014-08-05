ded72375c889a2efc67089a64e817346ed33b9d5
Lewis Jang <Lewis.AEdes@gmail.com>
2014-08-05 15:21:03 +0800

    補足 filter 測試器

--- src/Aedes/MotoBundle/Tests/Helper/FilterHelperTest.php
+++ src/Aedes/MotoBundle/Tests/Helper/FilterHelperTest.php
@@ -28,4 +28,51 @@ class FilterHelperTest extends WebTestCase
 
         $this->assertSame($sameFilter, $return);
     }
+
+    public function testBetween()
+    {
+        $between = array(
+            'years',
+            'price'
+        );
+
+        $request = array(
+            'yearsMin' => 1,
+            'yearsMax' => 2,
+            'priceMax' => 50
+        );
+
+        // TODO: 這裡有測試 bug ，因為條件其實先後順序是無差的，但 assertSame 會完全相等，需改成 is in array 然後用 assertTrue 來測試
+        $return = array(
+            'o.years <= \'2\'',
+            'o.years >= \'1\'',
+            'o.price <= \'50\'',
+        );
+
+        $betweenFilter = Filter::between("o", $between, $request);
+
+        $this->assertSame($betweenFilter, $return);
+    }
+
+    public function testLike()
+    {
+        $like = array(
+            'title',
+            'content'
+        );
+
+        $request = array(
+            'title' => 'some title',
+            'content' => 'some content'
+        );
+
+        $return = array(
+            'o.title LIKE \'%some title%\'',
+            'o.content LIKE \'%some content%\''
+        );
+
+        $likeFilter = Filter::like("o", $like, $request);
+
+        $this->assertSame($likeFilter, $return);
+    }
 }
